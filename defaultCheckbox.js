f27526bf93468cb06736bc7d0be72104eb69f651
hakanbaser <hakan.baser@se-co.de>
2016-12-06 09:29:25 +0100

    add a new scipts and updates scripts

--- defaultCheckbox.js
+++ defaultCheckbox.js
@@ -1,29 +1,56 @@
 var defaultChecklist=[
 {
-  id:"Retrospective",
+  id:"Retro",
   label:"Retrospective happens after every sprint",
   category: "RE",
   SubCategory: false,
+  factor:{ TR:1 , AL:0 , TS:1 , CV:0, EF:0 
   },
+},
 
-  {
-  id:"proposals",
+{
+  id:"RetroPro",
   label:"Results in concrete improvement proposals",
   category: "RE",
   SubCategory: true,
+  factor:{ TR:1 , AL:1 , TS:1 , CV:1, EF:0 
   },
+},
 
-  {
+{
+  id:"RetroProImp",
+  label:"Some proposals actually get implemented",
+  category: "RE",
+  SubCategory: true,
+  factor:{ TR:0 , AL:0 , TS:1 , CV:0, EF:1 
+  },
+},
+
+/*
+{
+  id:"RetroPoTeam",
+  label:"Whole team + PO participates",
+  category: "RE",
+  SubCategory: true,
+  factor:{ TR:1 , AL:0 , TS:1 , CV:1, EF:0 
+  },
+},
+
+{
   id:"POBacklog",
   label:"PO has a product backlog (PBL)",
   category: "PBL",
   SubCategory: false,
- },
+  factor:{ TR:0 , AL:0 , TS:1 , CV:1, EF:1 
+  },
+},
 
- {
+{
   id:"SprintPlanningMeeting",
   label:"Have sprint planning meetings",
   category: "SPM",
   SubCategory: false,
- }
+  factor:{ TR:1 , AL:1 , TS:1 , CV:0, EF:0 
+  },
+}*/
 ]
--- initCheckbox.js
+++ initCheckbox.js
@@ -1,9 +1,16 @@
+
+var factorsMax={TR:0,AL:0,TS:0,CV:0,EF:0}
+var factorSubFactors={TR:0,AL:0,TS:0,CV:0,EF:0}
+var factorsSumAll={TR:0,AL:0,TS:0,CV:0,EF:0}
+// var output = [], itemHa;
 function initCheckbox(){
   var myTable=document.getElementById("boxen")
-
+  
   for (var i in defaultChecklist)
   {
     var item=defaultChecklist[i]
+    defineFactorAttribution(item)
+
     var myRow=myTable.insertRow(-1)
     var myTH1=document.createElement("th")
     myTH1.innerHTML='<input type="checkbox" onclick="checkBoxVisible(this);" " name="'+ item.id+'"></input>' + ' ' + item.label
@@ -14,18 +21,19 @@ function initCheckbox(){
     }
     myRow.appendChild(myTH1)
   }
+
+  inhalt()
 }
 
 function checkBoxVisible(cat){
-
  var targetVisibility=(cat.checked)?"visible":"hidden"
+ var chekboxStat = (cat.checked)?1:0
  var myCat =  getCheckBoxID (cat)
  setVisibility (myCat, targetVisibility)
+ SumOfFactors(cat, chekboxStat)
 }
 
-
 function getCheckBoxID (cat) {
-
   for (var x in defaultChecklist)
   { 
     var all = defaultChecklist[x]
@@ -35,8 +43,36 @@ function getCheckBoxID (cat) {
   }
 }
 
-function setVisibility (cat, targetVisibility) {
+function defineFactorAttribution (checkItem) {
+  for (var x in checkItem.factor)
+  { 
+    factorsMax[x] += checkItem.factor[x]
+  }
+  return factorsMax
+}
 
+function SumOfFactors(cat, checkStatus){
+  for (var x in defaultChecklist) {
+   all = defaultChecklist[x]
+   if (cat.name == all.id) {
+    for (var y in all.factor) {
+     factorSubFactors[y] = all.factor[y]
+     if (checkStatus) {
+       factorsSumAll[y]  += checkStatus * factorSubFactors[y]
+     }
+      else
+     {
+        factorsSumAll[y]  -= factorSubFactors[y]
+     }
+    // alert("Status " + checkStatus  +  " factor " + factorSubFactors[y] + " Summe " + factorsSumAll[y] + " FactorMax " + factorsMax[y] + " Ergebnis " + factorsSumAll[y]/factorsMax[y])
+  //   alert("Status " + checkStatus  + " FactorMax " + factorsMax[y] + " Ergebnis " + factorsSumAll[y]/factorsMax[y])
+     }
+
+    }
+  }
+}
+
+function setVisibility (cat, targetVisibility) {
   for (var x in defaultChecklist)
   { 
     var all = defaultChecklist[x]
@@ -44,9 +80,25 @@ function setVisibility (cat, targetVisibility) {
       document.getElementById("ROW-"+all.id).style.visibility = targetVisibility
     }
   }
-
 }
 
+// Test
 
+function inhalt(){
+
+defaultChecklist.forEach(function(m,i){
+
+alert("checklist " + i)
+
+for(ein in m){
+
+  alert(m[ein])
+
+  }
+
+});
+}
+
+//TestEnde
 
 initCheckbox()
